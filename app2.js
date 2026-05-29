00ea96c680b8f076b243a20ae0d8d90ff65d7ed6
Gonzalo Costela <gonzalocostela@gmail.com>
2026-05-29 05:14:09 +0000

    feat: Improve screen messages using assigning function in section title

--- app2.js
+++ app2.js
@@ -2,6 +2,7 @@
 const listaAmigosSorteados = []; // Registra nombres sorteados para comparar con nombres ingresado
 const listaAmigosIngresados = []; // Registra nombres ingresados
 let nombreAmigo = ""; // Variable que almacena temporalmente el nombre ingresado
+let mensaje = ""; // Variable global almacena temporalmente mensajes para el usuario
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
@@ -16,18 +17,23 @@ function agregarAmigo() {
 
     // Verificar que el campo no este vacio
     if (nombreAmigo === '') {
-        alert("Por favor inserte un nombre");
+        mensaje = "Por favor inserte un nombre válido";
+        asignarTextoElemento('.section-title', mensaje);
         return;
     }
     
     // Verificar que no existan duplicados, si existe mostrar alerta
     if (listaAmigosIngresados.includes(nombreAmigo)) {
-        alert('ERROR: ese nombre ya existe, intente nuevamente');
+        mensaje = 'ERROR: ese nombre ya existe, intente nuevamente';
+        asignarTextoElemento('.section-title', mensaje);
     
     // Si no hay error, se actualiza el Array y se borra el nombre ingresado del Input   
     } else {
         listaAmigosIngresados.push(nombreAmigo);
         actualizarLista();
+
+        mensaje = "Digite el nombre de sus amigos"
+        asignarTextoElemento('.section-title', mensaje);
     }
     limpiarNombre();
 }
@@ -54,18 +60,21 @@ function actualizarLista() {
 function sortearAmigo() {
     // Verifica si existen nombres por sortear, de lo contrario dispara un alerta
     if (listaAmigosIngresados.length === 0) {
-        alert("No quedan amigos por sortear.");
+        mensaje = "No quedan amigos por sortear.";
+        asignarTextoElemento('.section-title', mensaje)
         return null;
     }
     // Generación de numeros aletorios basado en indice de los elementos
     const indiceAleatorio = Math.floor(Math.random() * listaAmigosIngresados.length);
+    
     // Elimina el amigo sorteado del Array de amigos ingresados
     const amigoSorteado = listaAmigosIngresados.splice(indiceAleatorio, 1)[0];
     listaAmigosSorteados.push(amigoSorteado); // Registro historico de elementos sorteados
 
     // Actualizar lista de nombres restantes, y visualizacion de ultimo nombre sorteado
     actualizarLista();
-    alert(`Amigo sorteado: ${amigoSorteado}`);
+    mensaje = `Amigo sorteado: ${amigoSorteado}`;
+    asignarTextoElemento('section-title', mensaje);
 
     // Verificación en consola para pruebas técnicas
     console.log("Amigos restantes:", listaAmigosIngresados);
